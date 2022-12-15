844767fc282ee561c328a36a06d89b765a23453d
Andriinef <andrii.nefedov@nure.ua>
2022-12-15 12:26:03 +0200

    first commit

---
+++ src/config/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.contrib import admin
+from django.http import HttpResponse
+from django.urls import include, path, re_path
+from django.views.static import serve
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),
+    re_path(r"^images/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+    path("", include("apps.urls")),  # apps
+]
